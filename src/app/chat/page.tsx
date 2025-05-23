581991fd50aba6f0b017eb0332fe8dc5ccf81130
ajeeshRS <ajeeshrs79@gmail.com>
2025-05-23 16:05:59 +0530

    feat:  add redirect to login page if not logged in

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,7 +3,9 @@
 import { supabase } from "@/utils/supabaseClient";
 import { useEffect } from "react";
 import ChatInnerLayout from "@/components/layout-chat";
+import { useRouter } from "next/navigation";
 export default function Page() {
+  const router = useRouter();
 
   useEffect(() => {
     const getUser = async () => {
@@ -12,6 +14,9 @@ export default function Page() {
       } = await supabase.auth.getSession();
 
       console.log(session?.user);
+      if (!session?.user) {
+        router.push("/login");
+      }
     };
     getUser();
   }, []);
